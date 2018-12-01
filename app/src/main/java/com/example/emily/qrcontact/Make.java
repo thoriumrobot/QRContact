a781eb61ff4684eb0a5a7363b5c0b0ad29e69aac
emilyvera <evera5@illinois.edu>
2018-12-01 17:25:18 -0600

    Added create page

--- app/src/main/java/com/example/emily/qrcontact/Make.java
+++ app/src/main/java/com/example/emily/qrcontact/Make.java
@@ -5,14 +5,40 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 public class Make extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make);
+        Picasso.get().setLoggingEnabled(true);
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        Button refresh = (Button) findViewById(R.id.button);
+        final EditText firstName = (EditText) findViewById(R.id.firstName);
+        final EditText lastName = (EditText) findViewById(R.id.lastName);
+        final EditText phone = (EditText) findViewById(R.id.phone);
+        final EditText email = (EditText) findViewById(R.id.email);
+
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("myTag", "Registered Click");
+                Picasso.get().load("https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=" + "BEGIN:VCARD VERSION:2.1 N:"
+                        + lastName.getText() + ";" + firstName.getText() + " " + "FN:" + firstName.getText() + " " + lastName.getText()
+                        + " EMAIL;TYPE=INTERNET:" + email.getText() + " TEL;TYPE=voice,cell,pref:" + phone.getText() + " END:VCARD" ).into(imageView);
+
+            }
+        });
+        loadImage(imageView);
 
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigation);
         Menu menu = bottomNavigationView.getMenu();
@@ -38,4 +64,9 @@ public class Make extends AppCompatActivity {
             }
         });
     }
+    private void loadImage(ImageView toSet) {
+        Log.d("fucntion", "Default code");
+        Picasso.get().load("https://api.qrserver.com/v1/create-qr-code/?size=300x300&data=Example").into(toSet);
+    }
+
 }
